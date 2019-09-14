c0bcbe5da7cbcbd348550949c0d2eaa5de52427f
Aaron Clare <AaronClare@A.local>
2019-09-14 13:39:05 -0700

    added game elements, styled cards, and added Pokemon header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
+import pokelogo from './poke.png';
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <div className="App-img">
+        <img className="App-logo" src={pokelogo} alt="Pokemon Logo"/>
+      </div>
+      <Pokegame />
     </div>
   );
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
+const PokeAPI = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number);
 
 
 
 class Pokecard extends Component{
     render(){
+        let imgSrc = `${PokeAPI}${padToThree(this.props.id)}.png`;
         return(
             <div className="Pokecard">
-                <h3 className="Pokecard-name">Name: {this.props.name}</h3>
-                <img className="Pokecard-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`}/>
-                <h3 className="Pokecard-type">Type: {this.props.type}</h3>
-                <h3 className="Pokecard-xp">Base Experience: {this.props.base_experience}</h3>
+                <h1 className="Pokecard-name">{this.props.name}</h1>
+                <div className="Pokecard-img">
+                    <img src={imgSrc} alt={this.props.name}/>
+                </div>
+                <h3 className="Pokecard-data">Type: {this.props.type}</h3>
+                <h3 className="Pokecard-data">Base Experience: {this.props.base_experience}</h3>
             </div>
         )
     }
